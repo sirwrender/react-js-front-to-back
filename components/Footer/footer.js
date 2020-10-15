d14506b3427f3bbc78f67528dacdb443008568ab
Rishabh Mishra <rishabhm1974@gmail.com>
2020-10-15 14:29:06 +0530

    added footer header and hero section

---
+++ components/Footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+  return (
+    <footer class="footer"></footer>
+  )
+}
+
+
+export default Footer;
---
+++ components/Navbar/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './navbar.css'
+
+
+function Navbar() {
+  return (
+    <nav class="navbar">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Projects</a></li>
+      <li><a href="#">Contact</a></li>
+    </nav> 
+  )
+}
+
+export default Navbar;
